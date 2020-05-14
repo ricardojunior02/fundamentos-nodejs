ae78257b724a2fcebef37650bd4342989df9d832
ricardojunior02 <ricardojunior199502@gmail.com>
2020-05-14 18:34:14 -0300

    Adiciona Metodos de Listagem e Criação

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,13 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface CreateTransactionDTO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +21,48 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    const listTransaction = this.transactions;
+
+    return listTransaction;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transation: Transaction) => {
+        switch (transation.type) {
+          case 'income':
+            accumulator.income += transation.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transation.value;
+            break;
+          default:
+            break;
+        }
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const createTransaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(createTransaction);
+
+    return createTransaction;
   }
 }
 
